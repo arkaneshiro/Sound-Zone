3ac31ec29aad5494e332487abe358e064a322b17
Riki Kaneshiro <arkaneshiro@gmail.com>
2020-08-16 17:05:58 -0700

    form code cleanup

--- src/components/session/RegistrationForm.js
+++ src/components/session/RegistrationForm.js
@@ -22,48 +22,40 @@ const RegistrationForm = ({ registerUser, updateImg, previewImgUrl }) => {
         <div className={styles.formContainer}>
             <h1>Sign Up</h1>
             <form className={styles.form} onSubmit={handleSubmit(formSubmitter)}>
-                {/* <label className={styles.label} htmlFor="username" >Username: */}
-                    <input
-                        className={styles.textInput}
-                        type="text"
-                        id="username"
-                        name="username"
-                        placeholder="username"
-                        ref={register({ required: true })}
-                    />
-                {/* </label> */}
+                <input
+                    className={styles.textInput}
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="username"
+                    ref={register({ required: true })}
+                />
                 {errors.username && <div className={styles.error}>username required</div>}
-                {/* <label className={styles.label} htmlFor="email" >Email: */}
-                    <input
-                        className={styles.textInput}
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="email"
-                        ref={register({ required: true })}
-                    />
-                {/* </label> */}
+                <input
+                    className={styles.textInput}
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="email"
+                    ref={register({ required: true })}
+                />
                 {errors.email && <div className={styles.error}>email required</div>}
-                {/* <label className={styles.label} htmlFor="password" >Password: */}
-                    <input
-                        className={styles.textInput}
-                        type="text"
-                        id="password"
-                        name="password"
-                        placeholder="password"
-                        ref={register({ required: true })}
-                    />
-                {/* </label> */}
+                <input
+                    className={styles.textInput}
+                    type="text"
+                    id="password"
+                    name="password"
+                    placeholder="password"
+                    ref={register({ required: true })}
+                />
                 {errors.password && <div className={styles.error}>password required</div>}
-                {/* <label className={styles.label} htmlFor="bio" >Bio: */}
-                    <textarea
-                        className={styles.textAreaInput}
-                        id="bio"
-                        name="bio"
-                        placeholder="bio"
-                        ref={register({ required: true })}
-                    />
-                {/* </label> */}
+                <textarea
+                    className={styles.textAreaInput}
+                    id="bio"
+                    name="bio"
+                    placeholder="bio"
+                    ref={register({ required: true })}
+                />
                 {errors.bio && <div className={styles.error}>bio required</div>}
                 <div className={styles.imageUpload}>
                     <img className={styles.imagePreview} src={previewImgUrl} alt='avi preview' />
@@ -76,7 +68,12 @@ const RegistrationForm = ({ registerUser, updateImg, previewImgUrl }) => {
                         />
                 </div>
                 <LabelButton labelfor='submit-sign-up' innerhtml='Sign Up'/>
-                <input className={styles.submitInput} type="submit" id='submit-sign-up' value="Sign Up" />
+                <input
+                    className={styles.submitInput}
+                    type="submit"
+                    id='submit-sign-up'
+                    value="Sign Up"
+                />
             </form>
         </div>
     )
