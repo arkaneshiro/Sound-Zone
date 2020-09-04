8e2f827c42765c28e06f95644768f67140bf2ec5
Riki Kaneshiro <arkaneshiro@gmail.com>
2020-09-03 18:43:45 -0700

    cleanup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,7 +21,7 @@ const NavBar = ({searchData, currentUserId, logouter}) => {
     }
 
 
-    // changes what nav is displaye based on if ur logged in
+    // changes what nav is displayed based on if ur logged in
     const nav = currentUserId ? (
         <div className={styles.navBar}>
                 <div className={styles.linkContainer}>
@@ -84,95 +84,3 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// -------    first attempt at search filter -----------
-
-
-
-
-    // const { handleSubmit, register, watch } = useForm();
-    // const [displaySearch, setDisplaySearch] = useState(false);
-
-
-    // // search functions
-    // const onSearch = (data, e) => {
-    //     console.log(watch('searchResults'))
-    //     e.target.reset()
-    // }
-
-    // const changer = e => {
-    //     // console.log(e.target.value)
-
-    // }
-
-    // const openResults = () => {
-    //     setDisplaySearch(true)
-    // }
-
-
-    // // filters search results based off of what you type in input
-    // const searchResults = searchData ?
-    //         searchData.map((user) => {
-    //             if (watch('search')) {
-    //                 if (user.username.toLowerCase().includes(watch('search').toLowerCase())) {
-    //                     if (user.id !== parseInt(currentUserId)) {
-    //                         return (
-    //                             <option value={user.id} key={user.id}>
-    //                                 {user.username}
-    //                             </option>
-    //                             )
-    //                     } else {
-    //                         return ""
-    //                     }
-    //                 } else {
-    //                     return ""
-    //                 }
-    //             } else {
-    //                 return ""
-    //             }
-    //         }
-    //     )
-    //     :
-    //     ""
-
-// this was in the form element
-/* <input
-    className={styles.searchBar}
-    type="search"
-    id="search"
-    name="search"
-    placeholder="search for a user"
-    autoComplete="off"
-    onFocus={openResults}
-
-    ref={register()}
-    />
-<select
-    className={styles.searchResultsContainer}
-    name="searchResults"
-    hidden={!displaySearch}
-    onChange={changer}
-    ref={register()}
-    >
-    <option value='nope'>
-        ---- pick one ----
-    </option>
-    {searchResults}
-</select> */
